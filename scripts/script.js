dd5a3a45cbb55614b71d2fa6d7281f11f92ef77f
shoproizoshlo <iamsusunny@gmail.com>
2023-10-03 17:26:42 +0200

    added toggle menu to navbar

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// click on the menu button in mobile version
 document.getElementById("menu-icon").addEventListener("click", () => {
   if (document.getElementById("menu").style.display == "block") {
     document.getElementById("menu").style.display = "none";
@@ -10,4 +11,43 @@ document.getElementById("menu-icon").addEventListener("click", () => {
       '<img onclick="teste()" src="./images/icon-close-menu.svg" alt="">';
     document.querySelector(".overlay").style.display = "block";
   }
+});
+
+// click on the menu toggle buttons
+const toggleButtons = document.querySelectorAll(".icon-arrow-down");
+const toggleLine = document.querySelectorAll(".q-line");
+const toggleTexts = document.querySelectorAll(".toggle-text");
+// const questionTexts = document.querySelectorAll(".question");
+
+let currentOpenButton = null;
+let currentOpenLine = null;
+
+toggleLine.forEach((line, index) => {
+  line.addEventListener("click", () => {
+    const targetId = line.getAttribute("data-target");
+    const targetElement = document.getElementById(targetId);
+    targetElement.style.maxHeight = "150px";
+
+    if (currentOpenLine && currentOpenLine !== line) {
+      const currentTargetId = currentOpenLine.getAttribute("data-target");
+      const currentTargetElement = document.getElementById(currentTargetId);
+
+      currentTargetElement.classList.remove("open");
+      currentOpenLine.classList.remove("closing");
+      currentTargetElement.style.maxHeight = "0";
+      toggleButtons.forEach((button) => {
+        button.classList.remove("closing");
+      });
+    }
+
+    if (targetElement.classList.contains("open")) {
+      targetElement.classList.remove("open");
+    } else {
+      targetElement.classList.add("open");
+    }
+
+    toggleButtons[index].classList.toggle("closing");
+
+    currentOpenLine = line;
+  });
 });
